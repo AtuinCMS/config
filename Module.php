7f2410f11cd74a2f7052ebc46d22d9882f50d7cf
Paroklo <patroklo@gmail.com>
2015-08-15 17:09:20 +0200

    Changes

--- Module.php
+++ Module.php
@@ -9,7 +9,7 @@ class Module extends \atuin\skeleton\Module
 
     protected static $_id = 'config';
 
-    protected static $_version = '0';
+    protected static $_version = '0.0.1';
 
     public $is_core_module = 1;
 }
